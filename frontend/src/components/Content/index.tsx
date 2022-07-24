f7ff7ed8b731d8b54613cf65c962fe6ed34c24b5
Dekopossas <andrepossaster@gmail.com>
2022-07-24 18:43:03 -0300

    components

---
+++ frontend/src/components/Content/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Content() {
+  return (
+    <Container>
+      <div>Content</div>
+    </Container>
+  );
+}
+
+export default Content;
---
+++ frontend/src/components/MainHeader/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function MainHeader() {
+  return (
+    <Container>
+      <div>MainHeader</div>
+    </Container>
+  );
+}
+
+export default MainHeader;
