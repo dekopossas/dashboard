3cc7b113eea7fd5135e487b5ecbf85387135a10a
Dekopossas <andrepossaster@gmail.com>
2022-07-24 21:34:50 -0300

    w3 school emojis' git push

--- frontend/src/components/MainHeader/index.tsx
+++ frontend/src/components/MainHeader/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
-import { Container } from './styles';
+import emojis from '../../utils/emojis';
+
+import { Container, Profile, UserName, Welcome } from './styles';
 
 function MainHeader() {
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length);
+    return emojis[indice];
+  }, []);
+
   return (
     <Container>
-      <h1>MainHeader</h1>
+      <h1>Toogle</h1>
+
+      <Profile>
+        <Welcome>Olá, {emoji}</Welcome>
+        <UserName>André Possas</UserName>
+      </Profile>
     </Container>
   );
 }
