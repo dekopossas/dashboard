60581ad4eafb37b07d50e6f4174e9bc48c37b2e2
Dekopossas <andrepossaster@gmail.com>
2022-07-29 15:48:00 -0300

    typing sjusts

--- frontend/src/components/ContentHeader/index.tsx
+++ frontend/src/components/ContentHeader/index.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
-import Selectinput from '../../components/Selectinput';
 
 import { Container, TitleContainer, Controllers } from './styles';
 
 interface IContentHeaderProps {
   title: string;
   lineColor: string;
-  controllers: React.ReactNode;
+  children: React.ReactNode;
 }
 
-function ContentHeader({ title, lineColor, controllers }: IContentHeaderProps) {
-  const options = [
-    { value: 'diana', label: 'Diana' },
-    { value: 'lais', label: 'Laís' },
-    { value: 'miriam', label: 'Míriam' },
-  ];
-
+function ContentHeader({ title, lineColor, children }: IContentHeaderProps) {
   return (
     <Container>
       <TitleContainer>
         <h1>{title}</h1>
       </TitleContainer>
       <Controllers>
-        {controllers}
+        {children}
       </Controllers>
     </Container>
   );
--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from 'react';
 import ContentHeader from '../../components/ContentHeader';
+import SelectInput from '../../components/SelectInput';
 
 import { Container } from './styles';
 
 function Dashboard() {
+  const options = [
+    { value: 'diana', label: 'Diana' },
+    { value: 'lais', label: 'Laís' },
+    { value: 'miriam', label: 'Míriam' },
+  ];
+
   return (
     <Container>
-      <ContentHeader>
-        
+      <ContentHeader title="DashBoard" lineColor={"#fff"}>
+        <SelectInput options={options} />
       </ContentHeader>
     </Container>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
