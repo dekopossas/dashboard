e50b257e801a9d3f3d85fe50d18958c32616de00
Dekopossas <andrepossaster@gmail.com>
2022-08-04 23:12:14 -0300

    remove color styled

--- frontend/src/components/HisrotyFinanceCard/index.tsx
+++ frontend/src/components/HisrotyFinanceCard/index.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { Container, Tag } from './styles';
 
 interface IHisrotyFinanceCardProps {
-  cardColor: string;
   tagColor: string;
   title: string;
   subtitle: string;
@@ -11,14 +10,13 @@ interface IHisrotyFinanceCardProps {
 }
 
 function HisrotyFinanceCard({
-  cardColor,
   tagColor,
   title,
   subtitle,
   amount,
 }: IHisrotyFinanceCardProps) {
   return (
-    <Container color={cardColor}>
+    <Container>
       <Tag color = {tagColor} />
         <div>
           <span>{title}</span>
