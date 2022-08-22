1ebbeccb47e402fff245862c76831868a13a9e94
Dekopossas <andrepossaster@gmail.com>
2022-08-22 14:22:12 -0300

    writing correction

--- frontend/src/components/WalletBox/index.tsx
+++ frontend/src/components/WalletBox/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Assets
-import dollarImg from '../../assets/dollar.svg';
+import dolarImg from '../../assets/dolar.svg';
 import arrowUpImg from '../../assets/arrow-up.svg';
 import arrowDownImg from '../../assets/arrow-down.svg';
 
@@ -21,7 +21,7 @@ function WalletBox({ title, amount, footerlabe, icon, color }: IWalletBoxProps) 
       <span>{title}</span>
       <h1>{amount}</h1>
       <small>{footerlabe}</small>
-      <img src={dollarImg} alt={title} />
+      <img src={dolarImg} alt={title} />
     </Container>
   );
 }
