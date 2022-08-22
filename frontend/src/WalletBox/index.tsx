23b1b4afec170e7261fd52eafba88a0f0e8b4dc7
Dekopossas <andrepossaster@gmail.com>
2022-08-22 14:16:29 -0300

    wallet box card done

--- frontend/src/WalletBox/index.tsx
+++ frontend/src/WalletBox/index.tsx
@@ -9,13 +9,21 @@ import { Container } from './styles';
 
 interface IWalletBoxProps {
   title: string;
-  amont: number;
+  amount: number;
   footerlabe: string;
   icon: 'dolar' | 'arrow-down' | 'arrow-up';
+  color: string;
 }
 
-function WalletBox() {
-  return <Container>WalletBox</Container>;
+function WalletBox({ title, amount, footerlabe, icon, color }: IWalletBoxProps) {
+  return (
+    <Container color={color}>
+      <span>{title}</span>
+      <h1>{amount}</h1>
+      <small>{footerlabe}</small>
+      <img src={dollarImg} alt={title} />
+    </Container>
+  );
 }
 
 export default WalletBox;
