322e7131530628a8f5dfa9d8214d77b482cceeae
Dekopossas <andrepossaster@gmail.com>
2022-08-26 00:02:37 -0300

    some styles

--- frontend/src/components/MessageBox/styles.ts
+++ frontend/src/components/MessageBox/styles.ts
@@ -7,6 +7,21 @@ export const Container = styled.div`
   background-color: ${(props) => props.theme.colors.tertiary};
   color: ${(props) => props.theme.colors.white};
 
+  border-radius: 7px;
+
   margin: 10px 0;
-  padding:30px;
+  padding: 30px 20px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  > header img {
+    width: 35px;
+    margin-left: 5px;
+  }
+  
+  > header p {
+    font-size: 18px;
+  }
 `;
