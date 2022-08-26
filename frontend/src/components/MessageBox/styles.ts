331698178862ba32d69b16204ffe9a0ab9b990ef
Dekopossas <andrepossaster@gmail.com>
2022-08-25 23:59:38 -0300

    some styles

--- frontend/src/components/MessageBox/styles.ts
+++ frontend/src/components/MessageBox/styles.ts
@@ -1,3 +1,12 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 48%;
+  height: 260px;
+
+  background-color: ${(props) => props.theme.colors.tertiary};
+  color: ${(props) => props.theme.colors.white};
+
+  margin: 10px 0;
+  padding:30px;
+`;
